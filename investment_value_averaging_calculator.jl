ded33c5259b6f171b1447a88152e6d7ae328f456
Donglai Gong <truedichotomy@gmail.com>
2021-08-19 15:45:54 -0400

    Update investment_value_averaging_calculator.jl

--- investment_value_averaging_calculator.jl
+++ investment_value_averaging_calculator.jl
@@ -2,26 +2,29 @@
 # Donglai Gong, 2021-08-19
 #
 # starting date 2021-08-19
-# ETF to invest: VOO (S&P 500 index fund from Vanguard)
+# ETFs to invest: VOO (S&P 500 index fund from Vanguard) and QQQ
 
-ticker = "VOO"
-investment_initial = 3634.56; # where we are starting from
-investment_annual = (100.0-9.0)*403.84 # aiming to gain 91 shares of VOO
+trading_periods_per_year = 52
+
+ticker = "VOO"  #2021-08-19
+investment_initial = 3637.98; # where we are starting from
+investment_annual = (100.0-9.0)*405   # aiming to gain 91 shares of VOO
 fund_historical_performance_frac = 0.16
-fund_added_performance_frac = 0.07
+fund_added_performance_frac = 0.10
 
-#ticker = "QQQ"
-#investment_initial = 7277.20; # where we are starting from
-#investment_annual = (100.0-20.0)*363.86 # aiming to gain 91 shares of VOO
-#fund_historical_performance_frac = 0.23
-#fund_added_performance_frac = 0.07
+# ticker = "QQQ" #2021-08-19
+# investment_initial = 5461.35+7275.6; # where we are starting from
+# investment_annual = (200.0-35.0)*365 # aiming to gain 91 shares of VOO
+# fund_historical_performance_frac = 0.23
+# fund_added_performance_frac = 0.07
 
-#investment_initial = 50000.0;
-#investment_annual = 6000.0;
+# ticker = "QQQ" #hypothetical
+# investment_initial = 100000; # where we are starting from
+# investment_annual = 5000 # aiming to gain 91 shares of VOO
+# fund_historical_performance_frac = 0.23
+# fund_added_performance_frac = 0.0
 
 principal = investment_initial+investment_annual
-
-trading_weeks_per_year = 52
 added_performance_frac = fund_added_performance_frac
 
 #investment_annual_boost_frac = investment_annual / investment_initial;
@@ -31,26 +34,25 @@ added_performance_frac = fund_added_performance_frac
 investment_goal = (investment_initial + investment_annual) * (1.0 + fund_historical_performance_frac);
 
 # this is the desired interest rate on investment on a weekly basis
-weekly_historical_rate = (1.0 + fund_historical_performance_frac) ^ (1/Float64(trading_weeks_per_year)) - 1.0;
-weekly_boosted_rate = added_performance_frac / (Float64(trading_weeks_per_year));
-weekly_rate = weekly_historical_rate + weekly_boosted_rate
+historical_rate = (1.0 + fund_historical_performance_frac) ^ (1/Float64(trading_periods_per_year)) - 1.0;
+boosted_rate = added_performance_frac / (Float64(trading_periods_per_year));
+target_rate = historical_rate + boosted_rate
 #weekly_rate = (fund_historical_performance_frac + added_performance_frac) / (Float64(trading_weeks_per_year))
 
 # calculate the investment amount and the total for each week during a year
-investment_now = Array{Float64}(undef,trading_weeks_per_year+1);
+investment_now = Array{Float64}(undef,trading_periods_per_year+1);
 investment_now[1] = investment_initial;
-investment_weekly_change = Array{Float64}(undef,trading_weeks_per_year+1);
-investment_weekly_change[1] = 0.0;
-trading_week = collect(0:trading_weeks_per_year);
+investment_target_change = Array{Float64}(undef,trading_periods_per_year+1);
+investment_target_change[1] = 0.0;
+trading_period = collect(0:trading_periods_per_year);
 
-for ii = 1:trading_weeks_per_year
-    investment_weekly_change[ii+1] = (investment_now[ii] * weekly_rate) + (investment_annual / trading_weeks_per_year);
-    investment_now[ii+1] = investment_now[ii] + investment_weekly_change[ii+1];
+for ii = 1:trading_periods_per_year
+    investment_target_change[ii+1] = (investment_now[ii] * target_rate) + (investment_annual / trading_periods_per_year);
+    investment_now[ii+1] = investment_now[ii] + investment_target_change[ii+1];
 end
 investment_final = investment_now[end]
 
 investment_performance = investment_final / principal
 
 # printout of the investment strategy for value averaging
-[trading_week investment_weekly_change investment_now]
-
+[trading_period round.(investment_target_change) round.(investment_now)];
